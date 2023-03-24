7af3f4f81ab1f64ed1fbab599c4f025ad0387642
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-03-24 13:09:07 -0700

    updated htmlrouters

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -6,7 +6,6 @@ router.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
-
 // Wildcard route to direct users to a 404 page
 router.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/pages/index.html"))
